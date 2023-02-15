f20eaa89536d2f2a7e50e2155d85d266a0e4994c
Viktoriia <vacalatyvika02@gmail.com>
2023-02-15 22:52:04 +0200

    'advance' level almost done

--- 28/script.js
+++ 28/script.js
@@ -10,30 +10,48 @@ const mapLetterToNumber = ["Третій", "Четвертий", "П'ятий", 
 let counterUl = 0;
 let valueLi = 8;
 const listAdd = document.querySelector('#list-add');
-listAdd.addEventListener('click', function (event) {
+
+function addList() {
     if (counterUl < 8) {
         const li = document.createElement('li');
         let valueUl = mapLetterToNumber[counterUl];
-            li.innerHTML = `<span class="hide">` +
-                `${valueUl} список</span>` +
-                `<ul>` +
-                `<li>${++valueLi} елемент списку</li>` +
-                `<li>${++valueLi} елемент списку</li>` +
-                `<li>${++valueLi} елемент списку</li>` +
-                `<li>${++valueLi} елемент списку</li>` +
-                `</ul>`
-            li.setAttribute("class", "maybeRemove");
-            list.appendChild(li);
+        li.innerHTML = `<span class="hide">` +
+            `${valueUl} список</span>` +
+            `<ul>` +
+            `<li>${++valueLi} елемент списку</li>` +
+            `<li>${++valueLi} елемент списку</li>` +
+            `<li>${++valueLi} елемент списку</li>` +
+            `<li>${++valueLi} елемент списку</li>` +
+            `</ul>`
+        li.setAttribute("class", "maybeRemove");
+        list.appendChild(li);
     }
-    counterUl++
-})
-const remove = document.querySelector('.maybeRemove');
-const listRemove = document.querySelector('#list-remove');
-// remove.addEventListener('dblclick', function(event){
-    listRemove.addEventListener('dblclick', function(event){
-    // console.log("li");
-remove.remove();
-})
+    counterUl++;
+
+    const removeItem = Array.prototype.slice.call(document.querySelectorAll('.maybeRemove')); // масив з li
+    console.log(removeItem);
+}
+
+// function deleteList(){
+//     for(let i =0; i< removeItem; i++){
+//         removeItem[i].remove();
+//     }
+//     console.log(removeItem);
+// }
+
+listAdd.addEventListener('click', addList);
+
+// for (let i = 0; i < 6; i++) {
+//     removeItem[i].addEventListener('dblclick', deleteList)
+// }
+
+// removeItem.addEventListener('dblclick', function (event) {
+//     console.log("double click");
+//     // listRemove.addEventListener('dblclick', function (event) {
+//     // console.log("li");
+//     // removeItem.remove();
+//     // }
+// })
 
 
 // Advanced: зробити видалення списку і врахувати коректність нумерації після видалення.
