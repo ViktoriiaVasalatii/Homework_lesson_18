4aed7753330a91b1b28abff5ec677ba89a913718
Viktoriia <vacalatyvika02@gmail.com>
2023-02-16 23:11:21 +0200

    removing thi first two items works

--- 28/script.js
+++ 28/script.js
@@ -1,4 +1,15 @@
 const list = document.querySelector('.list');
+const li = Array.prototype.slice.call(document.getElementsByTagName('li')); // масив з li вкладеного списку)
+// const removeItem = document.querySelectorAll('.maybeRemove');
+
+const removeItem = Array.prototype.slice.call(document.querySelectorAll('.maybeRemove')); // масив з li, які можна буде видаляти(1-го рівня)
+
+
+const mapLetterToNumber = ["Третій", "Четвертий", "П'ятий", "Шостий", "Сьомий", "Восьмий", "Дев'ятий", "Десятий"];
+let counterUl = 0;
+let valueLi = 8;
+const listAdd = document.querySelector('#list-add'); // кнопка 
+
 list.addEventListener('click', function (event) {
     const trigger = event.target.closest('span');
     if (trigger) {
@@ -6,52 +17,38 @@ list.addEventListener('click', function (event) {
     }
 });
 
-const mapLetterToNumber = ["Третій", "Четвертий", "П'ятий", "Шостий", "Сьомий", "Восьмий", "Дев'ятий", "Десятий"];
-let counterUl = 0;
-let valueLi = 8;
-const listAdd = document.querySelector('#list-add');
-
-function addList() {
-    if (counterUl < 8) {
-        const li = document.createElement('li');
-        let valueUl = mapLetterToNumber[counterUl];
-        li.innerHTML = `<span class="hide">` +
-            `${valueUl} список</span>` +
-            `<ul>` +
-            `<li>${++valueLi} елемент списку</li>` +
-            `<li>${++valueLi} елемент списку</li>` +
-            `<li>${++valueLi} елемент списку</li>` +
-            `<li>${++valueLi} елемент списку</li>` +
-            `</ul>`
-        li.setAttribute("class", "maybeRemove");
-        list.appendChild(li);
+for (let i = 0; i < li.length; i++) {
+    // let valueLi = li[i].innerHTML[0]; // повинен був зчитувати нумерацію 
+    function addList() {
+        if (counterUl < 8) {
+            const elementli = document.createElement('li');
+            let valueUl = mapLetterToNumber[counterUl];
+            elementli.innerHTML = `<span class="hide">` +
+                `${valueUl} список</span>` +
+                `<ul>` +
+                `<li>${++valueLi} елемент списку</li>` +
+                `<li>${++valueLi} елемент списку</li>` +
+                `<li>${++valueLi} елемент списку</li>` +
+                `<li>${++valueLi} елемент списку</li>` +
+                `</ul>`
+                elementli.setAttribute("class", "maybeRemove");
+            list.appendChild(elementli);
+            removeItem.push(elementli);
+        }
+        counterUl++;
     }
-    counterUl++;
 
-    const removeItem = Array.prototype.slice.call(document.querySelectorAll('.maybeRemove')); // масив з li
-    console.log(removeItem);
 }
-
-// function deleteList(){
-//     for(let i =0; i< removeItem; i++){
-//         removeItem[i].remove();
-//     }
-//     console.log(removeItem);
-// }
 
 listAdd.addEventListener('click', addList);
 
-// for (let i = 0; i < 6; i++) {
-//     removeItem[i].addEventListener('dblclick', deleteList)
-// }
 
-// removeItem.addEventListener('dblclick', function (event) {
-//     console.log("double click");
-//     // listRemove.addEventListener('dblclick', function (event) {
-//     // console.log("li");
-//     // removeItem.remove();
-//     // }
-// })
+for (let i = 0; i < removeItem.length; i++) {   //видаляє тільки створені елементи з самого початку 
+    removeItem[i].addEventListener('dblclick', () => {
+
+        removeItem[i].remove();
+    })
+}
 
 
 // Advanced: зробити видалення списку і врахувати коректність нумерації після видалення.
