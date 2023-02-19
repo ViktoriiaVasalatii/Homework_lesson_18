55f451957cc568cf3f839e4c15281c8bf29a0175
Viktoriia <vacalatyvika02@gmail.com>
2023-02-19 22:50:11 +0200

    fixed errors

--- 29/script.js
+++ 29/script.js
@@ -1,28 +1,29 @@
 const category = Array.prototype.slice.call(document.querySelectorAll(".category_items"));
 const goodsBlock = Array.prototype.slice.call(document.querySelectorAll(".goods_block_items"));
+const goodsBlockItem = Array.prototype.slice.call(document.querySelectorAll(".goods_item"));
 const aboutBlock = document.querySelector(".about_goods_block");
 const button = document.querySelector(".button_buy");
-
-console.log(category);
-console.log(goodsBlock);
-console.log(aboutBlock);
+const arrayImage = Array.prototype.slice.call(document.querySelectorAll(".image"));
+const arrayTitle = Array.prototype.slice.call(document.querySelectorAll(".title_items"));
 
 for (let i = 0; i < category.length; i++) {
     category[i].addEventListener("click", () => {
-        console.log("aiuerh");
-        goodsBlock[i].classList.remove('hide');
-    })
-}
-for (let i = 0; i < goodsBlock.length; i++) {
-    goodsBlock[i].addEventListener("click", () => {
-        console.log("eeeeeeeeeeee");
-        aboutBlock.classList.remove('hide');
+        goodsBlock[i].classList.toggle('hide');
     })
 }
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < goodsBlockItem.length; i++) {
+    goodsBlockItem[i].addEventListener("click", () => {
+        aboutBlock.classList.toggle('hide');
+        document.querySelector(".title_items_big").innerText = arrayTitle[i].innerText;
+        document.querySelector(".image_big").src = arrayImage[i].src;
+    })
     button.addEventListener("click", () => {
-        goodsBlock[i].classList.add("hide")
-        aboutBlock.classList.add("hide")
+        goodsBlock[i].classList.add("hide");
+        aboutBlock.classList.add("hide");
     })
 }
+
+button.addEventListener("click", () => {
+    alert("Товар куплений");
+})
